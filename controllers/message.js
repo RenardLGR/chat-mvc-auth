b5c7f91a0de3e416078c6ac77ffaa9bd8abd3b5d
Olivier Reynard <olivier.reynard17@gmail.com>
2022-09-09 05:14:12 +0200

    added trahs icon for connected user only, clean up code, css design

--- controllers/message.js
+++ controllers/message.js
@@ -3,32 +3,40 @@ const User = require('../models/User')
 
 module.exports = {
     getMessages: async (req,res)=>{
-        //console.log(req.user)
         try{
-            let messageItems = await Message.find({})
-            //{userId:req.user.id}
-            const users = await User.find({})
-            let cpyMessageItems = []
-            // const itemsLeft = await Messages.countDocuments({userId:req.user.id,completed: false})
-            messageItems.forEach(message => {
-                // console.log(message.userId);
-                // let filtered = users.filter(user => user._id == message.userId)
-                // console.log(filtered);
-                //console.log(users.find(el => el._id === message.userId));
-                // message.authorName = users.find(el => el._id == message.userId).userName
-                //message.authorName = 'jean'
-                // message['authorName'] = 'jean'
-                let temp = Object.assign({}, message)
-                temp.authorName = users.find(el => el._id == message.userId).userName
-                temp.profilePicture = users.find(el => el._id == message.userId).profilePicture
-                let date = ''+temp._doc.date
-                // date = date.replace('T', ' ').slice(0,-4)
-                temp._doc.date = date.slice(0, 25)
-                cpyMessageItems.push(temp)
-            })
+            // let messageItems = await Message.find({})
+            // //{userId:req.user.id}
+            // const users = await User.find({})
+            // let cpyMessageItems = []
+            // // const itemsLeft = await Messages.countDocuments({userId:req.user.id,completed: false})
+            // messageItems.forEach(message => {
+            //     let temp = Object.assign({}, message)
+            //     temp.authorName = users.find(el => el._id == message.userId).userName
+            //     temp.profilePicture = users.find(el => el._id == message.userId).profilePicture
+            //     let date = ''+temp._doc.date
+            //     // date = date.replace('T', ' ').slice(0,-4)
+            //     temp._doc.date = date.slice(0, 25)
+            //     cpyMessageItems.push(temp)
+            // })
+            //All of that code is simply to send to ejs a object mixing the id of the author (from the Message schema) to his display name and his profile picture (favorite color) from the User Schema
             //console.log(messageItems);
             //console.log(cpyMessageItems);
-            res.render('chatrooms.ejs', {messages: cpyMessageItems,  user: req.user})
+
+            let messageItems = await Message.find({}).lean() //.lean() gives us back a normal looking object instead of a mongo document object
+            const users = await User.find({}).lean() //.lean() gives us back a normal looking object instead of a mongo document object
+            messageItems.forEach(message => {
+                //the purpose of the following lines is to have the object we send back to ejs to have more suitable properties
+                message.authorDisplayName = users.find(el => el._id == message.userId).userName //add to the object the author display name from his id
+                message.authorProfilePic = users.find(el => el._id == message.userId).profilePicture //add to the object the author profile picture from his id
+                let d = ''+ message.date
+                d = d.slice(0, 25)
+                message.date = d //make the date more readable
+            })
+            //console.log(messageItems);
+            res.render('chatrooms.ejs', {messages: messageItems,  user: req.user}) //req.user is the connected user meaning req.user._id or req.user.id (equivalent) would give us back his id.
+            //meaning we can check user.id === message.userId and add a trash icon if so
+
+            // res.render('chatrooms.ejs', {messages: cpyMessageItems,  user: req.user}) 
         }catch(err){
             console.log(err)
         }
